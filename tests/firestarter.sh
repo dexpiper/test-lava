154ec84c4557dd8e57cbc7163b54be0d0ba89b34
Aleksei Kurilov <a.kurilov@yadro.com>
2022-05-04 13:57:46 +0300

    Remove typo2

--- tests/firestarter.sh
+++ tests/firestarter.sh
@@ -1,3 +1,3 @@
-clone https://github.com/tud-zih-energy/FIRESTARTER
+git clone https://github.com/tud-zih-energy/FIRESTARTER
 cd FIRESTARTER
 lava-test-case firestarter-test --shell ./FIRESTARTER -t 300
