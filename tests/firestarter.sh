0d0474f4581e6a3326209578342c1752b7d0930f
Aleksei Kurilov <a.kurilov@yadro.com>
2022-05-04 13:56:38 +0300

    Remove typo

--- tests/firestarter.sh
+++ tests/firestarter.sh
@@ -1,3 +1,3 @@
-cd clone https://github.com/tud-zih-energy/FIRESTARTER
+clone https://github.com/tud-zih-energy/FIRESTARTER
 cd FIRESTARTER
 lava-test-case firestarter-test --shell ./FIRESTARTER -t 300
